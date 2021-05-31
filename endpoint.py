a36b3fd41810b8675a5c96c13d356ffc48c05b45
Jan Konstant <456545@mail.muni.cz>
2021-05-31 18:05:20 +0200

    reimplemented endpoint.py with Flask

--- endpoint.py
+++ endpoint.py
@@ -1,65 +1,73 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import cgi
-import cgitb
 import configparser
-import json
+
+from flask.globals import request
 
 from src.DatabaseManager import DatabaseManager, supported_get_params
 from src.Serializer import Serializer
 
-
-def print_output(json, status="200 OK"):
-    print('Content-Type: application/json')
-    print('Status: ' + status)
-    print('')
-
-    print(json)
-    exit(0)
-
+from flask import Flask, jsonify
 
 debug = False
-if (debug):
-    cgitb.enable()
+app = Flask(__name__)
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-arguments = cgi.FieldStorage()
 
-# no parameters provided
-if (len(arguments.keys()) == 0 or not config['Endpoint'].getboolean('AllowParams')):
-    output = ""
-    with open(config['Common']['TargetJson'], 'r') as f:
-        output = f.read()
-    print_output(output)
+@app.route('/', methods=["GET"])
+def get_json():
+    # no parameters provided
+    if (not bool(request.args) or not config['Endpoint'].getboolean('AllowParams')):
+        output = ""
+        with open(config['Common']['TargetJson'], 'r') as f:
+            output = f.read()
+        return app.response_class(
+            response=output,
+            status=200,
+            mimetype='application/json'
+        )
 
-# some parameters provided: must validate
+    # some parameters provided: must validate first
+    params = {}
+    for key in request.args.keys():
+        if (not key in supported_get_params.keys()):
+            return jsonify(get_key_error()), 400
+        if (not supported_get_params[key]["validator"](request.args.get(key))):
+            return jsonify(get_value_error(key)), 400
 
-params = {}
-for key in arguments.keys():
-    if (not key in supported_get_params.keys()):
-        print_output(json.dumps({"error": "Unsupported parameter",
-                                 "message": "This parameter is not supported. See the docs for supported parameters."}), "400 Bad Request")
-    if (not supported_get_params[key]["validator"](arguments.getvalue(key))):
-        print_output(json.dumps({"error": "Unsupported parameter value", "message": "Incorrect value for parameter '" +
-                                 key + "': " + supported_get_params[key]["help"]}), "400 Bad Request")
+        params[key] = request.args.get(key)
 
-    params[key] = arguments.getvalue(key)
+    polls = []
+    last_update = ""
+    with DatabaseManager(config['Common']['DatabaseFile']) as db_manager:
+        polls = db_manager.get_polls(params)
+        last_update = db_manager.get_last_update()
 
-polls = []
-last_update = ""
-with DatabaseManager(config['Common']['DatabaseFile']) as db_manager:
-    polls = db_manager.get_polls(params)
-    last_update = db_manager.get_last_update()
+    metadata = {
+        "last_update": last_update
+    }
 
-metadata = {
-    "last_update": last_update
-}
+    serializer = Serializer()
+    result_json = serializer.get_json(polls, metadata)
 
-serializer = Serializer()
-result_json = serializer.get_json(polls, metadata)
+    return app.response_class(
+        response=result_json,
+        status=200,
+        mimetype='application/json'
+    )
 
-print_output(result_json)
 
-# src: https://techexpert.tips/apache/apache-enable-python-cgi/
+def get_key_error():
+    return {
+        "error": "Unsupported parameter",
+        "message": "This parameter is not supported. See the docs for supported parameters."
+    }
+
+
+def get_value_error(key):
+    return {
+        "error": "Unsupported parameter value",
+        "message": "Incorrect value for parameter '" + key + "': " + supported_get_params[key]["help"]
+    }
